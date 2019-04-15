f04e4a8d83a5a3d0d9339833509b776e78b46a36
Nicola Bertazzo <nicola.bertazzo@eng.it>
2019-04-15 13:59:48 +0200

    print changes

--- vars/stamp.groovy
+++ vars/stamp.groovy
@@ -3,5 +3,18 @@ def info(message) {
 }
 
 def lastStableVersion() {
-    echo "fullDisplayName: ${currentBuild.previousBuild.fullDisplayName}"
+    def changes = "Changes:\n"
+	build = currentBuild
+	while(build != null && build.result != 'SUCCESS') {
+    	changes += "In ${build.id}:\n"
+    	for (changeLog in build.changeSets) {
+        	for(entry in changeLog.items) {
+            	for(file in entry.affectedFiles) {
+                	changes += "* ${file.path}\n"
+            	}
+        	}
+	    }
+    	build = build.previousBuild
+	}
+	echo changes
 }
