138c7d7f47c9f2398853e1b8230c5eb57c933e65
Nicola Bertazzo <nicola.bertazzo@eng.it>
2019-05-02 16:07:07 +0200

    add proxy

--- src/eu/stamp/ci/GitHubClient.groovy
+++ src/eu/stamp/ci/GitHubClient.groovy
@@ -1,7 +1,5 @@
-@Grapes([
-  @Grab(group='org.eclipse.jgit', module='org.eclipse.jgit', version='5.2.0.201812061821-r')
-  @GrabConfig(systemProperties='httpProxy.host=proxy.eng.it,httpProxy.port=3128,httpsProxy.host=proxy.eng.it,httpsProxy.port=3128')
-])
+@GrabConfig(systemProperties='httpProxy.host=proxy.eng.it,httpProxy.port=3128,httpsProxy.host=proxy.eng.it,httpsProxy.port=3128')
+@Grab(group='org.eclipse.jgit', module='org.eclipse.jgit', version='5.2.0.201812061821-r')
 import org.eclipse.egit.github.core.PullRequest
 import org.eclipse.egit.github.core.Repository
 import org.eclipse.egit.github.core.client.GitHubClient
