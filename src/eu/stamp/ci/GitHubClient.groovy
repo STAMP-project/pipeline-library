0a4c092636c6ac7fa5f2552e3d8f7a457ac83efc
Nicola Bertazzo <nicola.bertazzo@eng.it>
2019-05-02 17:13:44 +0200

    remove proxy from script

--- src/eu/stamp/ci/GitHubClient.groovy
+++ src/eu/stamp/ci/GitHubClient.groovy
@@ -1,5 +1,4 @@
-@GrabConfig(systemProperties='httpProxy.host=proxy.eng.it,httpProxy.port=3128,httpsProxy.host=proxy.eng.it,httpsProxy.port=3128')
-@Grab(group='org.eclipse.jgit', module='org.eclipse.jgit', version='5.2.0.201812061821-r')
+@Grab('org.eclipse.jgit:org.eclipse.jgit:5.3.1.201904271842-r')
 import org.eclipse.egit.github.core.PullRequest
 import org.eclipse.egit.github.core.Repository
 import org.eclipse.egit.github.core.client.GitHubClient
@@ -10,4 +9,3 @@ void createPullRequest() {
     // client.setOAuth2Token();
     client.setHeaderAccept("application/vnd.github.machine-man-preview+json");
 }
-
