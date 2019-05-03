642a5fd6a8a5e374808f71ecf3c7f2f0e8888ba4
Nicola Bertazzo <nicola.bertazzo@eng.it>
2019-05-03 10:02:39 +0200

    p

--- src/eu/stamp/ci/GitHubClient.groovy
+++ src/eu/stamp/ci/GitHubClient.groovy
@@ -1,4 +1,4 @@
-package eu.stamp.ci
+package 
 
 @Grab('org.eclipse.mylyn.github:org.eclipse.egit.github.core:5.3.0.201903061415-rc1')
 import org.eclipse.egit.github.core.PullRequest
@@ -8,6 +8,7 @@ import org.eclipse.egit.github.core.service.RepositoryService
 
 void createPullRequest() {
     GitHubClient client = new GitHubClient();
-    // client.setOAuth2Token();
-    client.setHeaderAccept("application/vnd.github.machine-man-preview+json");
+    client.setOAuth2Token("prova");
+    RepositoryService repoService = new RepositoryService(client);
+    PullRequestService service = new PullRequestService(client);
 }
