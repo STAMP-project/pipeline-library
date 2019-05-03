039d927a1fd4d4f7f443a12fd6252642130ec7b5
Nicola Bertazzo <nicola.bertazzo@eng.it>
2019-05-03 10:28:47 +0200

    add package

--- src/eu/stamp/ci/GitHubClient.groovy
+++ src/eu/stamp/ci/GitHubClient.groovy
@@ -2,6 +2,7 @@ package eu.stamp.ci
 
 @Grab('org.eclipse.mylyn.github:org.eclipse.egit.github.core:5.3.0.201903061415-rc1')
 import org.eclipse.egit.github.core.PullRequest
+import org.eclipse.egit.github.core.service.PullRequestService
 import org.eclipse.egit.github.core.Repository
 import org.eclipse.egit.github.core.client.GitHubClient
 import org.eclipse.egit.github.core.service.RepositoryService
