1be68574c4669303abe29936fad554c744735808
Nicola Bertazzo <nicola.bertazzo@eng.it>
2019-05-03 10:52:28 +0200

    add src

--- src/eu/stamp/ci/Client.groovy
+++ src/eu/stamp/ci/Client.groovy
@@ -1,6 +1,7 @@
 package eu.stamp.ci
 
 @Grab('org.eclipse.mylyn.github:org.eclipse.egit.github.core:5.3.0.201903061415-rc1')
+@Grab(group='com.google.code.gson', module='gson', version='2.3.1')
 import org.eclipse.egit.github.core.PullRequest
 import org.eclipse.egit.github.core.service.PullRequestService
 import org.eclipse.egit.github.core.Repository
